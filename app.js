a9dfaf7c7e8e28cf339f6351b87d846b1ae90c56
Avi Ben David <avi@brandale.co.il>
2024-12-09 20:25:06 +0200

    added login feature

--- app.js
+++ app.js
@@ -3,30 +3,12 @@ const app = express()
 app.use(express.static('public'))
 const bodyParser = require('body-parser')
 app.use(bodyParser.urlencoded({extended : true}))
-function calc(x,y,operation){
-switch (operation) {
-case '0':
-return x+y
-case '1':
-return x-y
-case '2':
-return x*y
-case '3':
-return x/y
-default:
-return 'incalculable'
-}
-}
-app.post('/calc', function(req,res){
-var x = parseInt(req.body.x)
-var y = parseInt(req.body.y)
-var operation = req.body.operation
-var result = calc(x,y,operation)
-res.end(`<html>
-<body>
-The answer is ${result}.
-<a href="/index.html">reset</a>
-</body>
-</html>`)
+app.get('/login', function(req,res){
+var user = req.body.username
+var pass = req.body.password
+if (user == 'guest' & pass == '1234')
+    res.end('welcome guest')
+else
+    res.end('invalid username and/or password')
 })
 app.listen(880)
